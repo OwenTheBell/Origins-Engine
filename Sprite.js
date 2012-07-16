00393ce108b954f7eb9fde39e38529cf9c16693d
Owen Bell <owenalexbell@gmail.com>
2012-07-16 18:17:32 -0400

    Moved clickMap to intialization to try and combat the clickMap undefined error

--- Sprite.js
+++ Sprite.js
@@ -44,17 +44,35 @@ var Sprite = klass(function (left, top, image, id) {
 
 var clickSprite = Sprite.extend(function(left, top, image, id){
 	this.mouseLoc = null;
-	this.clickMap = [];
 	this.clicked = false;
 	this.mouseOver = false; //detect mouse position over sprite
+
+	this.clickMap = [];
+	
+	var canvas = document.createElement('canvas');
+	canvas.width = this.width();
+	canvas.height = this.height();
+	var ctx = canvas.getContext('2d');
+	ctx.drawImage(this.image, 0, 0);
+	var pixels = [];
+	try {
+		pixels = ctx.getImageData(0, 0, this.width(), this.height()).data;
+	} catch (e) {
+		console.log('ERROR: ' + this.id + ' failed to load image');
+	}
+	var col = 0, row = 0;
+	
+	for (var i = 0; i < pixels.length; i += 4){
+		row = Math.floor((i / 4) / this.width());
+		col = (i/4)	- (row * this.width());
+		if(!this.clickMap[col]) this.clickMap[col] = [];
+		this.clickMap[col][row] = pixels[i+3] == 0 ? 0 : 1;
+	}
+	console.log(this.id + ' has dimensions of (' + col + ', ' + row + ')');
 })
 	.methods({
 		update: function(){
 			if (this.parent.activeScreen){
-				if (this.clickMap.length == 0){
-					this.makeClickMap();
-				}
-				
 				if (this.clicked){
 					this.onClick();
 					this.clicked = false;
@@ -78,28 +96,6 @@ var clickSprite = Sprite.extend(function(left, top, image, id){
 		},
 		onClick: function(){
 		},
-		makeClickMap: function(){
-			//Using canvas create an 2d array of transparent vs nontransparent pixels
-			var canvas = document.createElement('canvas');
-			canvas.width = this.width();
-			canvas.height = this.height();
-			var ctx = canvas.getContext('2d');
-			ctx.drawImage(this.image, 0, 0);
-			
-			var pixels = [];
-			try {
-				pixels = ctx.getImageData(0, 0, this.width(), this.height()).data;
-			} catch (e){
-				console.log('ERROR: ' + this.id + ' failed to load image');
-			}
-			
-			for (var i = 0; i < pixels.length; i+=4){
-				var col = Math.floor((i / 4) / this.width());
-				var row = (i/4)	- (col * this.width());
-				if(!this.clickMap[row]) this.clickMap[row] = [];
-				this.clickMap[row][col] = pixels[i+3] == 0 ? 0 : 1;
-			}
-		}
 	});
 
 var screenChangeSprite = clickSprite.extend(function(left, top, image, id, targetScreen){
