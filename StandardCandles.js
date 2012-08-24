5dfafbde62ee0127f2ef639a43e1d8254467f486
Owen Bell <owenalexbell@gmail.com>
2012-08-24 14:51:04 -0400

    Actual demo build

--- StandardCandles.js
+++ StandardCandles.js
@@ -81,107 +81,109 @@ var StandardCandlesScreen = Screen.extend(function(id){
       this.supr();
       var that = this;
       
-      if (this.positioningFrames){
-      	for (x in this.mice){
-      		var mouse = this.mice[x];
-      		mouse.changeTop(mouse.top + this.targetPosition[x].top);
-      		mouse.changeLeft(mouse.left + this.targetPosition[x].left);
-      	}
-      	this.positioningFrames--;
-      	if (this.positioningFrames <= 0){
-      		for (i in this.mice){
-      			this.mice[i].timedScaleTo(this.scaleTime * g.fps, (parseInt(i)+1) * 0.2);
-      		}
-      	}
-      }
-      //respond to what sprites were clicked
-      if (this.confirmedSprite){
-        var confirmed = this.confirmedSprite;
-        if(confirmed instanceof moveableSprite){
-          if ((function(){
-            for(target in that.arachneTargets){
-              if (confirmed.id === that.arachneTargets[target].maus) return false;
-            }
-            return true;
-          })()){
-            var x = Math.floor((confirmed.width - (confirmed.width * confirmed.scale)) / 2);
-            var x = confirmed.left - x;
-            var y = Math.floor((confirmed.height - (confirmed.height * confirmed.scale)) / 2);
-            var y = confirmed.top - y;
-            this.prevTargetText.text = this.targetText.text;
-            this.targetText.text = Math.floor(this.baseDist / confirmed.scale) + 'k';
-            var sprite = new moveableSprite(
-              confirmed.id + 'selector',
-              x, y,
-              this.selectorUrl,
-              confirmed.zIndex);
-            sprite.classes.push('maus_selector');
-            this.arachneTargets.push({x: x, y: y, id: sprite.id, maus: confirmed.id, dist: this.targetText.text});
-            this.addSprite(sprite);
-          }
-        } else if (confirmed.id === 'intercept_button' && !this.intercepting){
-          //check to see if the selection array is in the correct order
-          var that = this;
-          if ((function() {
-            if (that.arachneTargets.length != that.mausCount) return false; 
-            var prev = 0;
-            for(target in that.arachneTargets){
-              target = that.arachneTargets[target];
-              if (prev == 0) prev = parseInt(target.dist);
-              else if (parseInt(target.dist) > prev) prev = parseInt(target.dist);
-              else return false;
-            }
-            return true;
-          })()){
-            this.spriteArray['arachne_icon'].moveTo(this.arachneTargets[this.nextTarget].x, this.arachneTargets[this.nextTarget].y, 1);
-            this.nextTarget++;
-            this.intercepting = true;
-          }
-          //if the array wasn't in order than clear all selectors
-          else {
-            for(target in this.arachneTargets){
-              delete this.spriteArray[this.arachneTargets[target].id];
-            }
-            this.arachneTargets = [];
-            this.targetText.text = '';
-            this.prevTargetText.text = '';
-          }
-
-        } else if (confirmed.id === 'undo_button' && !this.intercepting){
-          var target = this.arachneTargets[this.arachneTargets.length - 1];
-          delete this.spriteArray[target.id];
-          if (this.arachneTargets.length > 1){
-            this.targetText.text = this.arachneTargets[this.arachneTargets.length - 2].dist;
-            if (this.arachneTargets.length > 2){
-              this.prevTargetText.text = this.arachneTargets[this.arachneTargets.length - 3].dist;
-            } else {
-              this.prevTargetText.text = '';
-            }
-          } else {
-            this.targetText.text = '';
-          }
-          this.arachneTargets.splice(this.arachneTargets.length - 1, 1);
-        }
-      }
+      if(g.activeScreen === this.id){
+	      if (this.positioningFrames){
+	      	for (x in this.mice){
+	      		var mouse = this.mice[x];
+	      		mouse.changeTop(mouse.top + this.targetPosition[x].top);
+	      		mouse.changeLeft(mouse.left + this.targetPosition[x].left);
+	      	}
+	      	this.positioningFrames--;
+	      	if (this.positioningFrames <= 0){
+	      		for (i in this.mice){
+	      			this.mice[i].timedScaleTo(this.scaleTime * g.fps, (parseInt(i)+1) * 0.2);
+	      		}
+	      	}
+	      }
+	      //respond to what sprites were clicked
+	      if (this.confirmedSprite){
+	        var confirmed = this.confirmedSprite;
+	        if(confirmed instanceof moveableSprite){
+	          if ((function(){
+	            for(target in that.arachneTargets){
+	              if (confirmed.id === that.arachneTargets[target].maus) return false;
+	            }
+	            return true;
+	          })()){
+	            var x = Math.floor((confirmed.width - (confirmed.width * confirmed.scale)) / 2);
+	            var x = confirmed.left - x;
+	            var y = Math.floor((confirmed.height - (confirmed.height * confirmed.scale)) / 2);
+	            var y = confirmed.top - y;
+	            this.prevTargetText.text = this.targetText.text;
+	            this.targetText.text = Math.floor(this.baseDist / confirmed.scale) + 'k';
+	            var sprite = new moveableSprite(
+	              confirmed.id + 'selector',
+	              x, y,
+	              this.selectorUrl,
+	              confirmed.zIndex);
+	            sprite.classes.push('maus_selector');
+	            this.arachneTargets.push({x: x, y: y, id: sprite.id, maus: confirmed.id, dist: this.targetText.text});
+	            this.addSprite(sprite);
+	          }
+	        } else if (confirmed.id === 'intercept_button' && !this.intercepting){
+	          //check to see if the selection array is in the correct order
+	          var that = this;
+	          if ((function() {
+	            if (that.arachneTargets.length != that.mausCount) return false; 
+	            var prev = 0;
+	            for(target in that.arachneTargets){
+	              target = that.arachneTargets[target];
+	              if (prev == 0) prev = parseInt(target.dist);
+	              else if (parseInt(target.dist) > prev) prev = parseInt(target.dist);
+	              else return false;
+	            }
+	            return true;
+	          })()){
+	            this.spriteArray['arachne_icon'].moveTo(this.arachneTargets[this.nextTarget].x, this.arachneTargets[this.nextTarget].y, 1);
+	            this.nextTarget++;
+	            this.intercepting = true;
+	          }
+	          //if the array wasn't in order than clear all selectors
+	          else {
+	            for(target in this.arachneTargets){
+	              delete this.spriteArray[this.arachneTargets[target].id];
+	            }
+	            this.arachneTargets = [];
+	            this.targetText.text = '';
+	            this.prevTargetText.text = '';
+	          }
 	
-      //keep arachne moving if she has stopped and collected a mouse
-      if (this.intercepting && !this.spriteArray['arachne_icon'].moving) {
-        this.miceCollected.text = parseInt(this.miceCollected.text) + 1;
-        delete this.spriteArray[this.arachneTargets[this.nextTarget - 1].maus];
-        delete this.spriteArray[this.arachneTargets[this.nextTarget - 1].id];
-        if (this.nextTarget < this.arachneTargets.length) {
-          this.spriteArray['arachne_icon'].moveTo(this.arachneTargets[this.nextTarget].x, this.arachneTargets[this.nextTarget].y, 1);
-          this.nextTarget++;
-        } else {
-          this.spriteArray['arachne_icon'].moveToOrigin(1);
-          this.selectedArray = [];
-          this.arachneTargets = [];
-          this.targetText.text = '';
-          this.prevTargetText.text = '';
-          this.createMice();
-          this.nextTarget = 0;
-          this.intercepting = false;
-        }
+	        } else if (confirmed.id === 'undo_button' && !this.intercepting){
+	          var target = this.arachneTargets[this.arachneTargets.length - 1];
+	          delete this.spriteArray[target.id];
+	          if (this.arachneTargets.length > 1){
+	            this.targetText.text = this.arachneTargets[this.arachneTargets.length - 2].dist;
+	            if (this.arachneTargets.length > 2){
+	              this.prevTargetText.text = this.arachneTargets[this.arachneTargets.length - 3].dist;
+	            } else {
+	              this.prevTargetText.text = '';
+	            }
+	          } else {
+	            this.targetText.text = '';
+	          }
+	          this.arachneTargets.splice(this.arachneTargets.length - 1, 1);
+	        }
+	      }
+		
+	      //keep arachne moving if she has stopped and collected a mouse
+	      if (this.intercepting && !this.spriteArray['arachne_icon'].moving) {
+	        this.miceCollected.text = parseInt(this.miceCollected.text) + 1;
+	        delete this.spriteArray[this.arachneTargets[this.nextTarget - 1].maus];
+	        delete this.spriteArray[this.arachneTargets[this.nextTarget - 1].id];
+	        if (this.nextTarget < this.arachneTargets.length) {
+	          this.spriteArray['arachne_icon'].moveTo(this.arachneTargets[this.nextTarget].x, this.arachneTargets[this.nextTarget].y, 1);
+	          this.nextTarget++;
+	        } else {
+	          this.spriteArray['arachne_icon'].moveToOrigin(1);
+	          this.selectedArray = [];
+	          this.arachneTargets = [];
+	          this.targetText.text = '';
+	          this.prevTargetText.text = '';
+	          this.createMice();
+	          this.nextTarget = 0;
+	          this.intercepting = false;
+	        }
+	      }
       }
     }
   })
