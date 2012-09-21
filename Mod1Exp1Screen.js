9101cd3138397269b5e220ba5a48da5980a57813
Owen Bell <owenalexbell@gmail.com>
2012-09-21 01:07:09 -0400

    Early version of the first Mod1 Experiment

--- Mod1Exp1Screen.js
+++ Mod1Exp1Screen.js
@@ -1,4 +1,45 @@
+var specialA = 0.0001;
+
 var Mod1Exp1Screen = Screen.extend(function(id){
+  this.setup = false;
 })
   .methods({
+    placeStars: function(){
+      this.stars = [];
+      //pull out all the place holder sprites and replace them with what
+      //we actually want, the clickStars
+      for(id in this.spriteArray){
+        if(id.match('Variable') || id.match('Supernova')){
+          //console.log(id);
+          var sprite = this.spriteArray[id];
+          var top = Math.floor(Math.random() * g.origins.height);
+          var left = Math.floor(Math.random() * g.origins.width);
+          var temp = new clickStar(id, left, top, sprite.image.src, sprite.zIndex, 0);
+          delete this.spriteArray[id];
+          this.addSprite(temp);
+        }
+      }
+      this.setup = true;
+    },
+    update: function(){
+      this.supr();
+      if (!this.setup) this.placeStars();
+    }
+  });
+
+var clickStar = clickSprite.extend(function(id, left, top, image, zIndex, dis){
+  //distance of the star in parsecs
+  this.distance = Math.ceil(Math.random() * 10) + (dis * 10);
+  var year = 31556900000; //number of milliseconds in a year
+  //time is calculated in years
+  //the 3.26 converts the distance from parsecs to light years
+  var time = (5 + (dis * 10)) * 3.26;
+  var functionOfT = specialA * Math.pow(Math.E, specialA * time);
+  this.redShift = functionOfT * this.distance;
+  //console.log('%s has z of %s and r of %s', this.id, this.redShift, this.distance);
+})
+  .methods({
+    onClick: function() {
+      console.log('%s has z of %s and r of %s', this.id, this.redShift, this.distance);
+    }
   });
--- input.js
+++ input.js
@@ -5,93 +5,91 @@
  * updated by input which occurs independent of the game loop happening.
  */
 var inputState = {
-	
-	shiftDown: false,
-	
-	pressed: [],
-	
-	key: {
-		values: [],
-		press: false
-	},
-	
-	mouse: {
-		X: -1,
-		Y: -1,
-		click: false, //input rejected value
-		down: false
-	},
-	
-	getKey: function(){
-		var returnKey = helper.cloneObj(this.key);
-		returnKey.value = returnKey.values[0];
-		this.key.press = false;
-		return returnKey;
-	},
-	
-	getMouse: function(){
-		var returnMouse = helper.cloneObj(this.mouse);
-		if (this.mouse.click){
-			this.mouse.click = false;
-		}
-		return returnMouse;
-	}
+  shiftDown: false,
+  pressed: [],
+  
+  key: {
+    values: [],
+    press: false
+  },
+  
+  mouse: {
+    X: -1,
+    Y: -1,
+    click: false, //input rejected value
+    down: false
+  },
+  
+  getKey: function(){
+    var returnKey = helper.cloneObj(this.key);
+    returnKey.value = returnKey.values[0];
+    this.key.press = false;
+    return returnKey;
+  },
+  
+  getMouse: function(){
+    var returnMouse = helper.cloneObj(this.mouse);
+    if (this.mouse.click){
+      this.mouse.click = false;
+    }
+    return returnMouse;
+  }
 }
 
 $(document).keydown(function(e){
-	if (e.which == 16){
-		inputState.shiftDown = true;
-	} else {
-		if(!inputState.shiftDown && ((e.which >= 65) && (e.which <= 90))){
-			if (inputState.key.values.indexOf(e.which + 32) == -1){
-				inputState.key.values.push(e.which + 32);
-				if(inputState.key.values.length == 1){
-					inputState.key.press = true;
-				}
-			}
-		} else {
-			if (inputState.key.values.indexOf(e.which) == -1){
-				inputState.key.values.push(e.which);
-				if(inputState.key.values.length == 1){
-					inputState.key.press = true;
-				}
-			}
-		}
-	}
+  if (e.which == 16){
+    inputState.shiftDown = true;
+  } else {
+    if(!inputState.shiftDown && ((e.which >= 65) && (e.which <= 90))){
+      if (inputState.key.values.indexOf(e.which + 32) == -1){
+        inputState.key.values.push(e.which + 32);
+        if(inputState.key.values.length == 1){
+          inputState.key.press = true;
+        }
+      }
+    } else {
+      if (inputState.key.values.indexOf(e.which) == -1){
+        inputState.key.values.push(e.which);
+        if(inputState.key.values.length == 1){
+          inputState.key.press = true;
+        }
+      }
+    }
+  }
 });
 
 $(document).keyup(function(e){
-	//only disrupt the key press if it is the key registered as down
-	if (e.which == 16){
-		inputState.shiftDown = false;
-	} else if (inputState.key.values.indexOf(e.which) > -1){
-		var target = inputState.key.values.indexOf(e.which);
-		inputState.key.values.splice(target, 1);
-		inputState.key.press = true;
-	} else if (inputState.key.values.indexOf(e.which + 32) > -1){
-		var target = inputState.key.values.indexOf(e.which + 32);
-		inputState.key.values.splice(target, 1);
-		inputState.key.press = true;
-	}
+  //only disrupt the key press if it is the key registered as down
+  if (e.which == 16){
+    inputState.shiftDown = false;
+  } else if (inputState.key.values.indexOf(e.which) > -1){
+    var target = inputState.key.values.indexOf(e.which);
+    inputState.key.values.splice(target, 1);
+    inputState.key.press = true;
+  } else if (inputState.key.values.indexOf(e.which + 32) > -1){
+    var target = inputState.key.values.indexOf(e.which + 32);
+    inputState.key.values.splice(target, 1);
+    inputState.key.press = true;
+  }
 });
 
 //mouse input
 $(document).mousemove(function(e){
-	inputState.mouse.X = e.pageX;
-	inputState.mouse.Y = e.pageY;
+  inputState.mouse.X = e.pageX;
+  inputState.mouse.Y = e.pageY;
 });
 
 $(document).mousedown(function(e){
-	e.preventDefault(); //this prevents objects from getting highlighted and dragged
-	if (e.which == 1){
-		inputState.mouse.click = true;
-		inputState.mouse.down = true;
-	}
+  e.preventDefault(); //this prevents objects from getting highlighted and dragged
+  if (e.which == 1){
+    inputState.mouse.click = true;
+    inputState.mouse.down = true;
+  }
 });
 
 $(document).mouseup(function(e){
-	if (e.which == 1){
-		inputState.mouse.click = false;
-		inputState.mouse.down = false;
-	}
+  if (e.which == 1){
+    inputState.mouse.click = false;
+    inputState.mouse.down = false;
+  }
 });
